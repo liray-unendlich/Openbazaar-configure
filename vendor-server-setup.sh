29c9b6df5950a75b95ebc38ea009da4b0b3a7b47
liray-unendlich <unendlichkeit629@gmail.com>
2018-02-01 11:55:11 +0900

    openbazaar script fixed

--- vendor-server-setup.sh
+++ vendor-server-setup.sh
@@ -29,6 +29,7 @@ sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 wget https://github.com/phoreproject/openbazaar-go/releases/download/v1.0.1/openbazaar-go-linux-amd64
 mv openbazaar-go-linux-amd64 openbazaard
 chmod +x openbazaard
+sudo cp openbazaard /usr/local/bin/
 sleep 1
 echo '*** Done 1/4 ***'
 sleep 1
@@ -71,21 +72,21 @@ echo -n 'Dont worry about rpcuser~ errors.'
 rpcusr=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 20 | head -1)
 rpcpass=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 20 | head -1)
 
-echo -e "rpcuser=$rpcusr \nrpcpassword=$rpcpass \nrpcallowip=127.0.0.1 \nlisten=1 \nserver=1 \ndaemon=1 \nstaking=0 \nlogtimestamps=1 \nmaxconnections=256 \n" > ~/.phore/phore.conf
+echo -e "rpcuser=$rpcusr\nrpcpassword=$rpcpass\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nstaking=0\nlogtimestamps=1\nmaxconnections=256\n" > ~/.phore/phore.conf
 
 echo '*** Done 3/4 ***'
 echo '*** Start syncing and initialize openbazaard ***'
 phored -daemon
-./openbazaard init
+openbazaard init
 sed -i -e 's/"AcceptStoreRequests": false/"AcceptStoreRequests": true/g' .openbazaar2.0/config
-rpcusr=$(cat .phore/phore.conf | grep rpcuser | awk '{print substr($0,9)}')
-rpcpass=$(cat .phore/phore.conf | grep rpcpassword | awk '{print substr($0,13)}')
+#rpcusr=$(cat .phore/phore.conf | grep rpcuser | awk '{print substr($0,9)}')
+#rpcpass=$(cat .phore/phore.conf | grep rpcpassword | awk '{print substr($0,13)}')
 sed -i -e "s:phorerpc:$rpcusr:" .openbazaar2.0/config
 sed -i -e "s:rpcpassword:$rpcpass:" .openbazaar2.0/config
 echo 'Please wait for a minute. it will show you result of phore-cli getinfo'
 sleep 60
 phore-cli getinfo
 sleep 2
-./openbazaard start > openbazaard.log
+openbazaard start --verbose
 echo "Your openbazaar server is started!"
 echo '*** Done 4/4 ***'
