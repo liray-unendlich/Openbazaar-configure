d3868a597023ae476a80c83336994830433bb3b1
liray-unendlich <unendlichkeit629@gmail.com>
2018-02-02 08:51:11 +0900

    Update vendor-server-setup.sh

--- vendor-server-setup.sh
+++ vendor-server-setup.sh
@@ -8,7 +8,7 @@ echo 'This script will install all required updates & package for Ubuntu 16.04 !
 echo 'This script will install openbazaar server client and phored.'
 echo 'You can run this script on VPS only.'
 echo '****************************************************************************'
-sleep 3
+sleep 2
 echo '*** Step 1/4 ***'
 echo '*** Installing package ***'
 sleep 2
@@ -16,16 +16,15 @@ apt-get update -y
 apt-get upgrade -y
 apt-get dist-upgrade -y
 apt-get install -y nano htop git
-apt-get install -y software-properties-common
-apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev libevent-dev
-apt-get install -y libboost-all-dev
-apt-get install -y libminiupnpc-dev
-apt-get install -y autoconf
-apt-get install -y automake
-add-apt-repository ppa:bitcoin/bitcoin -y
-apt-get update -y
-apt-get dist-upgrade -y
-apt-get install -y libdb4.8-dev libdb4.8++-dev
+#apt-get install -y software-properties-common
+#apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev libevent-dev
+#apt-get install -y libboost-all-dev
+#apt-get install -y libminiupnpc-dev
+#apt-get install -y autoconf
+#apt-get install -y automake
+#add-apt-repository ppa:bitcoin/bitcoin -y
+#apt-get update -y
+#apt-get install -y libdb4.8-dev libdb4.8++-dev
 wget https://github.com/phoreproject/Phore/releases/download/v1.2.0.0/phore-1.1.0-x86_64-linux-gnu.tar.gz
 wget https://github.com/phoreproject/openbazaar-go/releases/download/v1.0.1/openbazaar-go-linux-amd64
 mv openbazaar-go-linux-amd64 openbazaard
@@ -48,30 +47,24 @@ limit ssh/tcp
 ufw logging on
 ufw --force enable
 ufw status
-sleep 1
 echo '*** Starting & configuring the wallet ***'
 sleep 2
 phored -daemon
-sleep 3
+sleep 2
 echo -n 'Dont worry about rpcuser~ errors.'
 rpcusr=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 20 | head -1)
 rpcpass=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 20 | head -1)
-
 echo -e "rpcuser=$rpcusr\nrpcpassword=$rpcpass\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nstaking=0\nlogtimestamps=1\nmaxconnections=256\n" > ~/.phore/phore.conf
-
 echo '*** Done 3/4 ***'
 echo '*** Start syncing and initialize openbazaard ***'
-#phored -daemon
 openbazaard init
 sed -i -e 's/"AcceptStoreRequests": false/"AcceptStoreRequests": true/g' .openbazaar2.0/config
 #rpcusr=$(cat .phore/phore.conf | grep rpcuser | awk '{print substr($0,9)}')
 #rpcpass=$(cat .phore/phore.conf | grep rpcpassword | awk '{print substr($0,13)}')
 sed -i -e "s:phorerpc:$rpcusr:" .openbazaar2.0/config
 sed -i -e "s:rpcpassword:$rpcpass:" .openbazaar2.0/config
-echo 'Please wait for a minute. it will show you result of phore-cli getinfo'
-sleep 3
-#phore-cli getinfo
-sleep 2
+echo '*** Starting OpenBazaard ***'
+sleep 1
 openbazaard start --verbose
 echo "Your openbazaar server is started!"
 echo '*** Done 4/4 ***'
